d1aca929aae3f94daafbf351d6145bb6d132cd40
Diablo0651 <165946883+Diablo0651@users.noreply.github.com>
2025-04-19 23:56:41 +0200

    commit

--- src/main/java/net/diablo/testmod/item/ModItems.java
+++ src/main/java/net/diablo/testmod/item/ModItems.java
@@ -1,12 +1,12 @@
 package net.diablo.testmod.item;
 
 import net.diablo.testmod.TestMod;
+import net.diablo.testmod.item.custom.ChiselItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
-import org.apache.http.conn.util.PublicSuffixList;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
@@ -17,7 +17,8 @@ public class ModItems {
     public static final RegistryObject<Item> RAW_ALEXANDRITE = ITEMS.register("raw_alexandrite",
             () -> new Item(new Item.Properties()));
 
-
+    public static final RegistryObject<Item> CHISEL = ITEMS.register("chisel",
+            () -> new ChiselItem(new Item.Properties().durability(32)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
